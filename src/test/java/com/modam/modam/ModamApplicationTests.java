11fe63ca46debc10a6c2f23e2feaa323a3378bd6
ka11y <elsword485667@gmail.com>
2025-05-23 20:00:22 +0900

    first commit

---
+++ src/test/java/com/modam/modam/ModamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modam.modam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
